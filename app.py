3dbf3222e9d6e5faea75083183be2ac32cb168f3
sofinan <sofinan@rambler.ru>
2021-05-24 09:11:14 +0000

    MongoDB compose file added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Qwerty123@localhost/hw_epam_db'
@@ -8,14 +9,21 @@ db =  SQLAlchemy(app)
 class Records(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
+
+def getData(searchStr):
+    recData = ()
+    # kind, collectionName, trackName, collectionPrice, trackPrice, primaryGenreName, trackCount, trackNumber, releaseDate
+    # get artistid all data
+     tmp = (requests.get('https://itunes.apple.com/search?term=' + str(searchStr))).json()
+    for rec in tmp['results']:
+        if rec["artistName"] == searchStr:
+            #get records of the Beatles
+            recdata.append((rec["artistId"],))  
+            return 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+     return render_template("index.html", records = getData('The Beatles'))
 
 # Start local project
 if __name__ == "__main__":
